e115220bc7922fd0ed9529a78e1e333ed75a1c3d
vedaRadev <ryancarmstrong97@gmail.com>
2024-12-11 12:40:38 -0500

    simple state machine for maze animation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
 from tkinter import Tk, BOTH, Canvas
 from typing import Callable
 import time
+from enum import Enum
 
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 RUNNING = True
 
+
 class Window:
     def __init__(self, width: int, height: int, on_close: Callable):
         self._width = width
         self._height = height
-
         self._root = Tk()
         self._root.title = "Maze Solver" # type: ignore
         self._root.protocol("WM_DELETE_WINDOW", on_close)
@@ -68,11 +69,11 @@ class Maze:
 
 
     def generate_cells(self):
-        self._cells = []
+        self.cells = []
         for y in range(self.num_rows):
-            self._cells.append([])
+            self.cells.append([])
             for _ in range(self.num_cols):
-                self._cells[y].append(Cell())
+                self.cells[y].append(Cell())
 
 
     # TODO check that the row, col pair is in bounds of the cells matrix
@@ -82,7 +83,7 @@ class Maze:
         cell_bottomright_x = cell_topleft_x + self.cell_width
         cell_bottomright_y = cell_topleft_y + self.cell_height
         return (cell_topleft_x, cell_topleft_y, cell_bottomright_x, cell_bottomright_y)
-
+    
 
     def draw_move(self, window: Window, a_row: int, a_col: int, b_row: int, b_col: int, is_undo = False):
         color = "gray" if is_undo else "red"
@@ -100,7 +101,7 @@ class Maze:
     def draw_cells(self, window: Window):
         for r in range(self.num_rows):
             for c in range(self.num_cols):
-                cell = self._cells[r][c]
+                cell = self.cells[r][c]
                 tl_x, tl_y, br_x, br_y = self.get_cell_screen_coords(r, c)
                 cell.draw(window, "red", tl_x, tl_y, br_x, br_y)
 
@@ -108,6 +109,13 @@ class Maze:
 def on_window_close():
     global RUNNING
     RUNNING = False
+
+
+class SimulationState(Enum):
+    BEGIN = "begin"
+    BREAKING_ENTRANCE = "breaking entrance",
+    BREAKING_EXIT = "breaking exit",
+    DONE = "done"
 
 
 def main():
@@ -123,9 +131,28 @@ def main():
     num_rows = (SCREEN_HEIGHT - maze_offset) // cell_size
     num_cols = (SCREEN_WIDTH - maze_offset) // cell_size
     maze = Maze(maze_offset, maze_offset, num_rows, num_cols, cell_size, cell_size)
+    current_state = SimulationState.BEGIN
     while RUNNING:
         window.process_events()
         window.clear_color("black")
+
+        # TODO cleaner state machine implementation
+        match current_state:
+
+            case SimulationState.BEGIN:
+                # just for leaving one frame before breaking
+                current_state = SimulationState.BREAKING_ENTRANCE
+
+            case SimulationState.BREAKING_ENTRANCE:
+                maze.cells[0][0].has_top_wall = False
+                current_state = SimulationState.BREAKING_EXIT
+
+            case SimulationState.BREAKING_EXIT:
+                maze.cells[maze.num_rows - 1][maze.num_cols - 1].has_bottom_wall = False
+
+            case SimulationState.DONE:
+                pass # maze solved, nothing to do
+
         maze.draw_cells(window)
         time.sleep(0.05)
 
